7c8908880fb1773ad5aab7d72dc7805c389fa24a
kuwingto <kuwingto>
2019-11-15 17:43:43 +0800

    init

---
+++ cppClientPythonServerExample/cppClientPythonServerExample.cpp
@@ -0,0 +1,101 @@
+// cppClientPythonServerExample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string.h>
+#include <stdio.h>
+#include <chrono>  // for high_resolution_clock
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <zmq.hpp>
+#include "segment_service_input.pb.h"
+
+int main()
+{
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+	zmq::context_t m_context = zmq::context_t(1);
+	zmq::socket_t *m_pSock;
+
+	/****** start service **********/
+	m_pSock = new zmq::socket_t(m_context, ZMQ_REQ);
+	m_pSock->connect("tcp://localhost:5555");
+	int linger = 0;
+	m_pSock->setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
+	
+	cv::Mat img;
+	img = cv::imread("0036.bmp", CV_LOAD_IMAGE_GRAYSCALE);
+	
+	segment_service_input sendPack;
+	sendPack.set_class_id(0);
+	segment_service_input_Mat sendMat;
+	sendMat.set_width(img.size().width);
+	sendMat.set_height(img.size().height);
+	sendMat.set_image_data((char *)img.data, sizeof(uchar) * img.size().width * img.size().height);
+	std::string s1 = sendMat.SerializeAsString();
+	std::cout << "s1.size() = " << s1.size() << "\n";
+	sendPack.set_allocated_img(&sendMat);
+	std::string s = sendPack.SerializeAsString();
+	std::cout << "s.size() = " << s.size() << "\n";
+	/***********************
+	//  Send
+	**********************/
+	{
+		zmq::message_t message(s.size());
+		memcpy(message.data(), s.data(), s.size());
+		m_pSock->send(message);
+	}
+	sendPack.release_img();
+	/***********************
+	// get reply
+	**********************/
+	zmq::pollitem_t items[] = { { *m_pSock, 0, ZMQ_POLLIN, 0 } };
+	zmq::poll(&items[0], 1, 5 * 1000);
+
+	if (items[0].revents & ZMQ_POLLIN)
+	{
+		zmq::message_t reply;
+		if (m_pSock->recv(&reply, 0))
+		{
+			
+			std::string  msgStr = std::string((char*)reply.data(), reply.size());
+			//std::cout << msgStr << "\n";
+			
+			//unserialize
+			segment_service_input out;
+			out.ParseFromString(msgStr);
+
+			int class_id = out.class_id();
+			std::cout << "class_id = " << class_id << "\n";
+			int width = out.img().width();
+			int height = out.img().height();
+			std::cout << "width = " << width << " height = " << height << "\n";
+			cv::Mat img = cv::Mat(height,width, CV_8UC1);
+			memcpy(img.data, &out.img().image_data()[0], sizeof(uchar) * width * height);
+
+			cv::imshow("img",img);
+			cv::waitKey(1);
+		}
+		else
+		{
+			std::cout << "reply from server malformed! \n";
+			return -1;
+		}
+	}
+	else
+	{
+		std::cout << "No response from server \n";
+		m_pSock->close();
+		m_pSock = new zmq::socket_t(m_context, ZMQ_REQ);
+		m_pSock->connect("tcp://localhost:5555");
+		int linger = 0;
+		m_pSock->setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
+		
+		return -1;
+	}
+
+	std::cout << "press to continue \n";
+	delete m_pSock;
+	getchar();
+    return 0;
+}
+
---
+++ cppClientPythonServerExample/segmentationServer.py
@@ -0,0 +1,74 @@
+import os
+import math
+import random
+import numpy as np
+import cv2
+import importlib.util
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+import time
+
+import zmq
+import segment_service_input_pb2
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+
+class segmentationServer:
+    def __init__(self):
+        self.setupNetwork()
+        print("Ready")
+    def setupNetwork(self):
+        # setup your network here. 
+        self.a = 1
+        
+        
+if __name__ == '__main__' :
+    from time import time
+    ser = segmentationServer()
+    
+     # Here For demonstration, we read a image to the buffer and will send back to CLient
+    outimage = cv2.imread("./segmentation.bmp", cv2.IMREAD_GRAYSCALE)
+    
+    while True:
+        print("waiting connection")
+        message = socket.recv()
+        print("message recv")
+        if (message):
+            start = time()
+            
+            #input 
+            segment_service_input = segment_service_input_pb2.segment_service_input()
+            segment_service_input.ParseFromString(message)
+            print("class_id = " , segment_service_input.class_id)
+            print("image width = ", segment_service_input.img.width)
+            print("image height = ", segment_service_input.img.height)
+            #print(type(segment_service_input.img.image_data))
+            nparr = np.frombuffer(segment_service_input.img.image_data,dtype=np.ubyte)
+            print("data shape = ",nparr.shape)
+            img = nparr.reshape(segment_service_input.img.height,segment_service_input.img.width)
+            print("image shape = ",img.shape)
+            
+            #uncomment show the image
+            #print(img.shape)
+            #stacked_img = np.stack((img,)*3, axis=-1)
+            #imgplot = plt.imshow(stacked_img)
+            #plt.show()
+            
+            #handling 
+            
+            
+            #output
+            segment_service_output = segment_service_input_pb2.segment_service_input()
+            segment_service_output.class_id = segment_service_input.class_id
+            segment_service_output.img.width = segment_service_input.img.width
+            segment_service_output.img.height = segment_service_input.img.height
+            segment_service_output.img.image_data = outimage.tobytes() 
+            str = segment_service_output.SerializeToString()
+            socket.send(str)
+        else:
+            print("error message")
